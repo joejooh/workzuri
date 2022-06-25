733b38101afb7569f4719cf67fec4f14b942815e
joejooh <105309088+joejooh@users.noreply.github.com>
2022-06-25 13:04:51 -0400

    Add files via upload

---
+++ mycalculator.js
@@ -0,0 +1,14 @@
+var num1 = window.prompt("enter a number ")
+var  num2 = window.prompt("enter another number")
+
+num1 = parseFloat(num1);
+num1 = parseFloat(num2);
+
+document.write(num1 + num2);
+document.write(num1 - num2);
+document.write(num1 * num2);
+document.write(num1 / num2);
+
+
+
+
